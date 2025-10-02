5389f2162d61c1471ceb9f0521bf3b86f33f11df
Rahul <rtvconcepts@gmail.com>
2025-10-02 16:41:12 -0400

    simple guis for functionality testing built

---
+++ src/main/java/edu/au/cpsc/module7/controller/EditCreateWindowController.java
@@ -0,0 +1,24 @@
+package edu.au.cpsc.module7.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class EditCreateWindowController {
+    @FXML
+    private Label itemCreateLabel, amountCreateLabel, categoryCreateLabel, noteCreateLabel;
+
+    @FXML
+    private TextField itemCreateTextfield, amountCreateTextField, categoryCreateTextField, noteCreateTextField;
+
+    @FXML
+    private RadioButton incomeRadioButton, expenseRadioButton;
+
+    private Button saveButton, cancelButton;
+
+
+}
+
+
--- src/main/java/edu/au/cpsc/module7/controller/ExpenseTrackerAppController.java
+++ src/main/java/edu/au/cpsc/module7/controller/ExpenseTrackerAppController.java
@@ -1,14 +1,34 @@
 package edu.au.cpsc.module7.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
 public class ExpenseTrackerAppController {
     @FXML
-    private Label welcomeText;
+    private Label currentBalanceLabel, totalIncomeLabel, totalExpensesLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label itemLabel, amountLabel;
+
+    @FXML
+    private Label itemDetailView, amountDetailView, categoryDetailView, noteDetailView;
+
+    @FXML
+    private Label itemInfoDetail, amountInfoDetail, categoryInfoDetail, noteInfoDetail;
+
+    @FXML
+    private Button editButton, deleteButton, exportButton, newButton;
+
+    @FXML
+    private MenuItem file, edit;
+
+//    @FXML
+
+
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
 }
