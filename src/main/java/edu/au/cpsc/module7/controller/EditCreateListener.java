426061f5a36b62f142ddae1dca556d5ade14cbb8
Rahul <rtvconcepts@gmail.com>
2025-10-04 22:23:43 -0400

    edit/create window work in progress

---
+++ src/main/java/edu/au/cpsc/module7/controller/EditCreateListener.java
@@ -0,0 +1,9 @@
+package edu.au.cpsc.module7.controller;
+
+import edu.au.cpsc.module7.model.Transaction;
+
+public interface EditCreateListener {
+    void onSave(Transaction transaction, boolean isNew);
+
+    default void onCancel(){}
+}
